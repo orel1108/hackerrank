e9746a889ef96c3ac16f00f094c4085f168fd94c
Roman Orlovskyi <r.orlovskyi@gmail.com>
2016-02-08 19:10:21 +0200

    Added python solution of Alternating Chars

--- practice/algorithms/strings/alternating_characters/alternating_characters.py
+++ practice/algorithms/strings/alternating_characters/alternating_characters.py
@@ -1,27 +1,10 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-#include <string>
-
-using namespace std;
-
-int main()
-{
-    int t;
-    string s;
-    cin >> t;
-    while (t--)
-    {
-        cin >> s;
-        const size_t len = s.length();
-        
-        auto it = unique(s.begin(), s.end());
-        s.resize(distance(s.begin(), it));
-        
-        cout << (len - s.length()) << endl;
-    }
-    return 0;
-}
-
+t = int(raw_input())
+for _ in range(t):
+    s = raw_input()
+    last = ''
+    cnt = 0
+    for CHR in s:
+        if CHR != last:
+            last = CHR
+            cnt += 1
+    print len(s) - cnt
