22743a17b7770231c64888fc4140ced2aea46fd4
Roman Orlovskyi <r.orlovskyi@gmail.com>
2016-09-19 22:33:38 +0300

    Proprocessor Solution problem solved

---
+++ practice/cpp/introduction/preprocessor_solution.cpp
@@ -0,0 +1,15 @@
+#define foreach(v, i) for(int i = 0; i < v.size(); ++i)
+
+#define io(a) cin >> a
+
+#define FUNCTION(a, b)
+
+#define maximum(m, x) \
+    if (m < x) m = x
+
+#define minimum(m, x) \
+    if (m > x) m = x
+
+#define INF 1000000000
+
+#define toStr(a) #a
